44a8d34dfba09459f8d926ede7b1fb907279ac7f
dsavenk <denissavenkov@gmail.com>
2015-10-03 01:07:09 -0400

    Adds an app to do entity linking for all documents and cache the results

--- entity_linking.py
+++ entity_linking.py
@@ -3,10 +3,13 @@ from text2kb.websearch_features import WebSearchFeatureGenerator
 
 __author__ = 'dsavenk'
 
+import cPickle as pickle
+import functools
 import globals
 import logging
+import multiprocessing
 import sys
-from entity_linker.entity_linker import EntityLinker, KBEntity
+from entity_linker.entity_linker import EntityLinker
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s '
                            ': %(module)s : %(message)s',
@@ -14,22 +17,25 @@ logging.basicConfig(format='%(asctime)s : %(levelname)s '
 logger = logging.getLogger(__name__)
 
 
+def find_document_entities(document, parser, entity_linker):
+    content = document.content()
+    if len(content.strip()) > 0:
+        tokens = parser.parse(content).tokens
+        return entity_linker.identify_entities_in_document(tokens, min_surface_score=0.1)
+    return []
+
+
 def main():
     globals.read_configuration('config.cfg')
     entity_linker = EntityLinker.init_from_config()
     parser = CoreNLPParser.init_from_config()
-
     feature_generator = WebSearchFeatureGenerator.init_from_config()
-    doc = feature_generator.question_serps[u'what character did natalie portman play in star wars?'][10]
-    content = doc.content()
-
-    # while True:
-    # print "Please enter some text: "
-    # text = sys.stdin.readline().strip()
-    tokens = parser.parse(content).tokens
-    res = entity_linker.identify_entities_in_document(tokens)
-    for entity in res:
-        print entity
+    pool = multiprocessing.Pool(1)
+    processing = functools.partial(find_document_entities, parser=parser, entity_linker=entity_linker)
+    document_entities = pool.map(processing, [doc for serp in feature_generator.question_serps.itervalues()
+                                              for doc in serp][:10])
+    with open(sys.argv[1], 'w') as out:
+        pickle.dump(document_entities, out)
 
 
 if __name__ == "__main__":
